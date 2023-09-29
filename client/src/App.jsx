64ad8bf1acf509aadd935a5f7785eec1b514fb81
Julian Ordaz <1julianordaz@gmail.com>
2023-09-28 22:48:44 -0400

    fixed BrowserRouter module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Events from "./pages/Events";
 import Home from "./pages/Home";
@@ -9,15 +9,15 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-    <Router>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/merch" element={<Merch />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/merch" element={<Merch />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
