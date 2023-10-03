aa0980915b7624aa7c995610de0e542a22d6bf76
Julian Ordaz <1julianordaz@gmail.com>
2023-10-02 23:24:54 -0400

    formatting fix to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Contact from "./pages/Contact";
 import Events from "./pages/Events";
 import Home from "./pages/Home";
 import Merch from "./pages/Merch";
+
 import Navbar from "./components/Navbar";
 
 function App() {
