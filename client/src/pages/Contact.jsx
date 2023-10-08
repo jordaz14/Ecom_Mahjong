3dab8efd29452667eb2cf94b0e74d31bb80e5d33
Julian Ordaz <1julianordaz@gmail.com>
2023-10-07 23:49:52 -0400

    changed contact page to login page

--- client/src/pages/Contact.jsx
+++
@@ -1,8 +0,0 @@
-
-function Contact() {
-    return (
-        <h1 className="text-white font-bold text-center">Welcome to the Contact page</h1>
-    );
-  }
-
-export default Contact;
---
+++ client/src/pages/LogIn.jsx
@@ -0,0 +1,9 @@
+function LogIn() {
+  return (
+    <h1 className="text-white font-bold text-center">
+      Welcome to the Log-In page
+    </h1>
+  );
+}
+
+export default LogIn;
