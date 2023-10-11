e4120d1c72a8d5dcbb3f1dad3bbac13472b22197
Julian Ordaz <1julianordaz@gmail.com>
2023-10-10 21:40:12 -0400

    added check password toggle to login and register

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,33 +1,65 @@
+import React, { useState } from "react";
+
+import CreateCarousel from "../components/CreateCarousel.jsx";
 import TitleText from "../components/TitleText.jsx";
 import PageCarousel from "../components/PageCarousel.jsx";
 import ContentCard from "../components/ContentCard.jsx";
 
 function LogIn() {
+  const [ShowPassword, SetShowPassword] = useState("password");
+  const ShowPasswordToggle = () => {
+    if (ShowPassword == "password") {
+      SetShowPassword("text");
+    } else {
+      SetShowPassword("password");
+    }
+  };
   return (
     <>
-      <div className="h-screen snap-y snap-mandatory overflow-y-auto py-8">
+      <CreateCarousel>
         <PageCarousel className="md:w-2/3 mx-auto">
           <TitleText>Log in to your account</TitleText>
-        <ContentCard className="w-3/4 mx-auto py-2 px-4">
-        <form className="flex flex-col h-full">
-          <label for="email" className="text-left md:text-2xl">
-            Enter your email address:
-          </label>
-          <input type="email" id="Email" placeholder="user@email.com"></input>
-          <label for="email" className="text-left md:text-2xl">
-            Enter your password:
-          </label>
-          <input type="password" id="Password" placeholder="password"></input>
-          <input type="checkbox" id="CheckPassword" className="mt-2"></input>
-          <button className="bg-green-100 hover:bg-green-300 font-bold drop-shadow-lg mt-2 md:text-2xl">
-            Log-In
-          </button>
-        </form>
-        <p>Don't have an account? <a href=
-        "/register">Register Here</a></p>
-      </ContentCard>
+          <ContentCard className="w-3/4 mx-auto py-2 px-4">
+            <form className="flex flex-col h-full">
+              <label for="email" className="text-left md:text-2xl">
+                Enter your email address:
+              </label>
+              <input
+                type="email"
+                id="Email"
+                placeholder="user@email.com"
+              ></input>
+              <label for="email" className="text-left md:text-2xl">
+                Enter your password:
+              </label>
+              <input
+                type={ShowPassword}
+                id="Password"
+                placeholder="password"
+              ></input>
+              <div className="flex mt-2">
+                <input
+                  type="checkbox"
+                  id="CheckPassword"
+                  onClick={ShowPasswordToggle}
+                ></input>
+                <label for="checkbox" className="ml-2 text-left md:text-2xl">
+                  Show Password
+                </label>
+              </div>
+              <button className="bg-green-100 hover:bg-green-300 font-bold drop-shadow-lg mt-2 md:text-2xl">
+                Log-In
+              </button>
+            </form>
+            <p>
+              Don't have an account?{" "}
+              <a href="/register" className="font-bold">
+                Register
+              </a>
+            </p>
+          </ContentCard>
         </PageCarousel>
-      </div>
+      </CreateCarousel>
     </>
   );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
+
 import PageCarousel from "../components/PageCarousel";
 import ContentCard from "../components/ContentCard";
 import TitleText from "../components/TitleText";
+import CreateCarousel from "../components/CreateCarousel";
 
 function Register() {
+  const [ShowPassword, SetShowPassword] = useState("password");
+  const ShowPasswordToggle = () => {
+    if (ShowPassword == "password") {
+      SetShowPassword("text");
+    } else {
+      SetShowPassword("password");
+    }
+  };
   return (
     <>
-      <div className="h-screen snap-y snap-mandatory overflow-y-auto py-8">
+      <CreateCarousel>
         <PageCarousel className="md:w-2/3 mx-auto">
           <TitleText>Create an account</TitleText>
           <ContentCard className="w-3/4 mx-auto py-2 px-4">
@@ -22,7 +33,7 @@ function Register() {
                 Enter your password:
               </label>
               <input
-                type="password"
+                type={ShowPassword}
                 id="password"
                 placeholder="password"
               ></input>
@@ -30,20 +41,33 @@ function Register() {
                 Confirm your password:
               </label>
               <input
-                type="password"
+                type={ShowPassword}
                 id="confirmpassword"
                 placeholder="confirm password"
               ></input>
+              <div className="flex mt-2">
+                <input
+                  type="checkbox"
+                  id="CheckPassword"
+                  onClick={ShowPasswordToggle}
+                ></input>
+                <label for="checkbox" className="ml-2 text-left md:text-2xl">
+                  Show Password
+                </label>
+              </div>
               <button className="bg-green-100 hover:bg-green-300 font-bold drop-shadow-lg mt-2 md:text-2xl">
                 Register
               </button>
             </form>
             <p>
-              Already have an account? <a href="/login">Log-In Here</a>
+              Already have an account?{" "}
+              <a href="/login" className="font-bold">
+                Log-In
+              </a>
             </p>
           </ContentCard>
         </PageCarousel>
-      </div>
+      </CreateCarousel>
     </>
   );
 }
