9c2af2fc45454ae04707181eeece7adc11bda021
Julian Ordaz <1julianordaz@gmail.com>
2023-10-10 21:38:20 -0400

    body text component

---
+++ client/src/components/BodyText.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function BodyText(props) {
+  return (
+    <p className={'text-gray-100 text-xl w-4/5 font-bold mx-auto my-auto md:w-full md:text-3xl lg:text-4xl ' +props.className}>{props.children}</p>
+  );
+}
+
+export default BodyText;
