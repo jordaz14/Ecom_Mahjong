b2dafe1a3a3493a241bdf1a2f15b701c214e4870
Julian Ordaz <1julianordaz@gmail.com>
2023-10-10 21:39:27 -0400

    responsive rehaul to nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,39 @@
+import Icon from "../components/Icon";
+
+import Cart from "../assets/Cart.png";
+import Menu from "../assets/Menu.png";
+
 function Navbar() {
   return (
     <>
-      <nav className="flex justify-around items-center w-2/3 mx-auto mt-2 bg-green-800 rounded-md border-solid border-2">
-        <a href="/" className="text-white">
-          HOME
-        </a>
-        <a href="/events" className="text-white">
-          EVENTS
-        </a>
-        <a href="/merch" className="text-white">
-          MERCH
-        </a>
-        <a href="/login" className="text-white">
-          LOG-IN
-        </a>
-      </nav>
+      <section id="Nav1" className="hidden md:block">
+        <nav className="flex items-center justify-around w-2/3 mx-auto mt-2">
+          <div className="flex justify-around items-center w-full mx-auto bg-green-800 rounded-md border-solid border-2">
+            <a href="/" className="text-white">
+              HOME
+            </a>
+            <a href="/events" className="text-white">
+              EVENTS
+            </a>
+            <a href="/merch" className="text-white">
+              MERCH
+            </a>
+            <a href="/login" className="text-white">
+              LOG-IN
+            </a>
+          </div>
+          <div className="bg-green-100 hover:bg-green-300 rounded-full w-6 h-6 flex md:h-10 md:w-10 ml-4">
+            <Icon img={Cart} />
+          </div>
+        </nav>
+      </section>
+      <section id="Nav2" className="block md:hidden">
+        <div className="bg-white w-screen h-screen hidden">Menu</div>
+        <nav className="flex justify-between mx-2 mt-2">
+          <Icon img={Menu} />
+          <Icon img={Cart} />
+        </nav>
+      </section>
     </>
   );
 }
