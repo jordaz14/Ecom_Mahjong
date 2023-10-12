781a41c654de8a508bbb5ac4226d8af45c0a352a
Julian Ordaz <1julianordaz@gmail.com>
2023-10-11 22:56:59 -0400

    minor edits to modal and icon component props/formatting

--- client/src/components/Icon.jsx
+++ client/src/components/Icon.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Icon({ img, className }) {
+function Icon({ img, className, onClick, onMouseEnter, onMouseLeave }) {
   return (
     <>
-    <div className={'bg-green-100 hover:bg-green-300 rounded-full w-10 h-10 flex ' + className}>
+    <div className={'bg-green-100 hover:bg-green-300 rounded-full w-10 h-10 flex ' + className} onClick={onClick} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <img src={img} className="w-3/5 h-3/5 mx-auto my-auto" />
     </div>
     </>
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -6,7 +6,7 @@ function Modal({ open, children, onClose }) {
         <div className="bg-black bg-opacity-40 fixed h-full w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
           <div className="fixed h-4/5 w-4/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <p>{children}</p>
-            <button className="text-black font-bold bg-green-200 mt-2 text-xl align-text-top md:text-2xl" onClick={onClose}>
+            <button className="text-black font-bold bg-green-100 hover:bg-green-300 mt-2 text-xl align-text-top md:text-2xl" onClick={onClose}>
             &nbsp; Exit &nbsp;
             </button>
           </div>
