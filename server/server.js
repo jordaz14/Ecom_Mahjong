4f1b9c068664f1cdb0f1379681d9e833542d42d9
Julian Ordaz <1julianordaz@gmail.com>
2023-10-20 23:13:00 -0400

    added register and login routes for backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json());
@@ -25,20 +27,69 @@ app.get("/test", (req, res) => {
   });
 });
 
+app.post("/user-login", (req, res) => {
+  const UserData = req.body;
+
+  // Check if UserData is valid
+  if (UserData.name == "" || UserData.password == "") {
+    return res.send({Message: "Please enter valid user info."});
+  }
+
+  // DB Queries
+  const QueryUser = "SELECT * FROM users WHERE email = $1";
+
+  // Check if UserData is valid
+  const CheckUserAndReturnToken = async () => {
+    const QueryUserData = await db.query(QueryUser, [UserData.email]);
+
+    if (QueryUserData.length == 0) {
+      return res.send({Message: "Account doesn't exist. Try creating an account"});
+    }
+
+    const MatchingPasswordCheck = await bcrypt.compare(
+      UserData.password,
+      QueryUserData[0].password
+    );
+
+    if (!MatchingPasswordCheck) {
+      return res.send({Message: "Passwords do not match. Try again"});
+    } else {
+      const Token = jwt.sign({ UserId: UserData.email }, "RANDOM-TOKEN");
+
+      res.send({ Message: "Login successful", Auth: UserData.email, Token });
+    }
+  };
+
+  CheckUserAndReturnToken();
+});
+
+// Route for users registering an account
 app.post("/user-register", (req, res) => {
   const UserData = req.body;
 
-  const CheckUser = 'SELECT * FROM users WHERE email = $1';
+  // Check if UserData is valid
+  if (UserData.name == "" || UserData.password == "") {
+    return res.send({Message: "Please enter valid user info."});
+  }
 
-  console.log(UserData.email)
-  console.log(db.query(CheckUser, [UserData.email]));
+  // DB Queries
+  const QueryUser = "SELECT * FROM users WHERE email = $1";
+  const InsertUser = "INSERT INTO users (email, password) VALUES ($1, $2)";
 
-  /*
-  const InsertUser = 'INSERT INTO users (email, password) VALUES ($1, $2)';
-  db.query(InsertUser, [UserData.email, UserData.password]);
-  */
+  // Check if UserData is valid
+  const CheckUserAndCreateAccount = async () => {
+    const QueryUserData = await db.query(QueryUser, [UserData.email]);
 
-  res.send("POST REQUEST SUBMITTED");
+    if (QueryUserData.length > 0) {
+      return res.send({Message:"Account already exists. Try logging in."});
+    } else {
+      const HashPassword = await bcrypt.hash(UserData.password, 10);
+      db.query(InsertUser, [UserData.email, HashPassword]);
+      return res.send({Message:"Account created. Welcome!"});
+    }
+  };
+
+  CheckUserAndCreateAccount();
 });
 
 app.listen(5000, () => {
