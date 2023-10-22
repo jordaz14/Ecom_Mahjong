13e63558c98792bdae708b7d4651889cf68a2bbf
Julian Ordaz <1julianordaz@gmail.com>
2023-10-21 22:55:39 -0400

    added myaccount route to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 import Login from "./pages/LogIn";
 import Events from "./pages/Events";
@@ -8,6 +10,7 @@ import Merch from "./pages/Merch";
 import Register from "./pages/Register";
 import Checkout from "./pages/Checkout";
 import Test from "./pages/Test";
+import MyAccount from "./pages/MyAccount";
 
 import Navbar from "./section/Navbar";
 
@@ -22,6 +25,7 @@ function App() {
           <Route path="/merch" element={<Merch />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/myaccount" element={<MyAccount />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/test" element={<Test />} />
         </Routes>
