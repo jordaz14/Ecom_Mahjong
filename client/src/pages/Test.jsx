fa64d328374055a8d76846edc5bb92b1b95c43ad
Julian Ordaz <1julianordaz@gmail.com>
2023-10-25 23:15:29 -0400

    test page, ignore

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 import CreateCarousel from "../components/CreateCarousel";
 import PageCarousel from "../components/PageCarousel";
 
 function Test() {
+  const Token = cookies.get("TOKEN");
+
   const [Request, GetRequest] = useState("Awaiting input");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const request = await axios.get("http://localhost:5000/test");
+        const request = await axios.get("http://localhost:5000/test", {
+          headers: {
+            authorization: Token,
+          },
+        });
         console.log(`Info about John: ${JSON.stringify(request.data.john)}`);
         GetRequest(JSON.stringify(request.data.john));
       } catch (error) {
