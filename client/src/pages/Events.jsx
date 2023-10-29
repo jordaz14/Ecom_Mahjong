15a387c0c4f56cc9d2fd326e63d34fc4bf44fdfb
Julian Ordaz <1julianordaz@gmail.com>
2023-10-29 14:05:32 -0400

    formatting cleanups to events component

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,11 +1,8 @@
 import CreateCarousel from "../components/CreateCarousel";
 import PageCarousel from "../components/PageCarousel";
 import TitleText from "../components/TitleText";
-import ContentCard from "../components/ContentCard";
-import Icon from "../components/Icon";
 import EventRegister from "../components/EventRegister";
 
-import Plus from "../assets/plus.png";
 function Events() {
 
   return (
@@ -14,13 +11,13 @@ function Events() {
         <PageCarousel>
           <TitleText>October 2023</TitleText>
           <section className="flex flex-col h-3/4 justify-around mt-8">
-           <EventRegister IDNum="1">
+           <EventRegister idNum="1">
            </EventRegister>
-           <EventRegister IDNum="2">
+           <EventRegister idNum="2">
            </EventRegister>
-           <EventRegister IDNum="3">
+           <EventRegister idNum="3">
            </EventRegister>
-           <EventRegister IDNum="4">
+           <EventRegister idNum="4">
            </EventRegister>
           </section>
         </PageCarousel>
